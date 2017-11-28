abb5b978160bf44fc44e7a801309314fc3b2427d
Sarah Pieren <32098863+PierenS-BPC@users.noreply.github.com>
2017-11-28 13:54:19 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Login System</title>
+</head>
+<body>
+
+<form action="login.php" method ="post">
+Username <input type="text" name="username">
+Password <input type="password" name="password">
+<input type="submit" value="Login">
+</form>
+<body>
+<html>
+
